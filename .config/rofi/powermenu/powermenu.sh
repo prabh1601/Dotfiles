4bc1e0d556b5625366a4c925ae8a951b7f9c3825
prabh <prabh1601@gmail.com>
2022-02-13 18:46:28 +0530

    added powermenu script

---
+++ .config/rofi/powermenu/powermenu.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+theme="full_alt"
+dir="/home/prabh/.config/rofi/powermenu/"
+rofi_command="rofi -theme $dir/$theme"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Random colors
+styles=($(ls -p --hide="colors.rasi" $dir/styles))
+color="${styles[$(( $RANDOM % 8 ))]}"
+sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	# Random colors
+	styles=($(ls -p --hide="colors.rasi" $dir/styles))
+	color="${styles[$(( $RANDOM % 8 ))]}"
+	sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+
+	yes="Yes"
+	no="No"
+	options="$yes\n$no"
+    rofi_confirm="rofi -theme $dir/confirm_alt"
+	choice=$(echo -e "$options" | $rofi_confirm -dmenu -selected-row 0)
+	echo $choice
+}
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$suspend\n$logout"
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "Yes" ]]; then
+			poweroff
+		else 
+			exit 0
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans = "Yes" ]]; then
+			reboot
+		else
+			exit 0
+        fi
+        ;;
+  #   $lock)
+		# if [[ -f /usr/bin/i3lock ]]; then
+		# 	i3lock
+		# elif [[ -f /usr/bin/betterlockscreen ]]; then
+		# 	betterlockscreen -l
+		# else 
+		# 	echo "Nothing found"
+		# fi
+  #       ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans = "Yes" ]]; then
+			# mpc -q pause
+			# amixer set Master mute
+			systemctl suspend
+        else
+        	exit 0
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans = "Yes" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+        else
+        	exit 0
+        fi
+        ;;
+esac
