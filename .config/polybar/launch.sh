b916244b49a5be6208aff1e306ab3b2157dc42b8
prabh <prabh1601@gmail.com>
2022-02-15 23:40:26 +0530

    add temporary volume

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,4 +4,4 @@
 killall -q polybar
 
 #Launch Polybar,with our config files
-polybar main -c /home/prabh/.config/polybar/config.polybar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main -c /home/prabh/.config/polybar/config.polybar 2>&1 | tee /tmp/polybar.log & disown
