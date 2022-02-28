ebdd488f162e10a7edae6b74aa929a91d6dc2937
prabh <prabh1601@gmail.com>
2022-02-28 12:53:30 +0530

    fixed scripts and changed css

--- .config/rofi/powermenu/logout.sh
+++ .config/rofi/powermenu/logout.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 
-theme="full_alt"
+theme="full_rounded"
 dir="/home/prabh/.config/rofi/powermenu/"
 rofi_command="rofi -theme $dir/$theme"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 # Random colors
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
-color="${styles[$(( $RANDOM % 6 ))]}"
+color="${styles[$(( $RANDOM % 5 ))]}"
 sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
 
 logout=""
 yes="Yes"
 no="No"
 options="$yes\n$no"
-rofi_confirm="rofi -theme $dir/confirm_alt"
-ans=$(echo -e "$options" | $rofi_confirm -p "$logout" -dmenu -selected-row 0)
+ans=$(echo -e "$options" | rofi -theme $dir/confirm_alt -p "$logout" -dmenu -selected-row 0)
 
 if [[ $ans = "Yes" ]]; then
-	if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-		openbox --exit
-	elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-		bspc quit
-	elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-		i3-msg exit
-	fi
+	i3-msg exit
 else
   	exit 0
 fi
