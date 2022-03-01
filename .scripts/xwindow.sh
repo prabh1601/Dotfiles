425b90132f137967c5f3f0999c205a20e5389334
prabh <prabh1601@gmail.com>
2022-03-01 19:09:00 +0530

    added xwindow module

---
+++ .scripts/xwindow.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+WM_DESKTOP=$(xdotool getwindowfocus)
+
+if [ $WM_DESKTOP == "4194404" ]; then
+	echo " Desktop "
+elif [ $WM_DESKTOP != "1883" ]; then
+	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
+	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
+
+	if [ "$WM_CLASS" == 'Enter WM_CLASS value here' ]; then
+		echo "Custom Name"
+	elif [ "$WM_NAME" == 'Enter WM_NAME value here' ]; then
+		echo "Custom Name"
+	else 
+		echo "$WM_CLASS"
+	fi
+fi
+
