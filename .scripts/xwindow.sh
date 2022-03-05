35f9176df735299352c61e765c46c88f3ce3c0d4
prabh <prabh1601@gmail.com>
2022-03-05 23:02:34 +0530

    fixed floating applications

--- .scripts/xwindow.sh
+++ .scripts/xwindow.sh
@@ -2,14 +2,14 @@
 WM_DESKTOP=$(xdotool getwindowfocus)
 
 if [ $WM_DESKTOP == "4194404" ]; then
-	echo " Desktop "
+	echo " Arch-Desktop "
 elif [ $WM_DESKTOP != "1883" ]; then
 	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
 	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
 
 	if [ "$WM_CLASS" == 'Enter WM_CLASS value here' ]; then
 		echo "Custom Name"
-	elif [[ "$WM_NAME" == "WhatsApp" || "$WM_NAME" == "Google Chrome" ]]; then
+	elif [[ "$WM_NAME" == "WhatsApp" || "$WM_NAME" == "Google Chrome" || "$WM_CLASS" == "Google-chrome" ]]; then
 		echo " $WM_NAME "
 	else 
 		echo "$WM_CLASS"
